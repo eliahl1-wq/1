5501bd3060207926fbac888e8a776beca597adf0
eliahl1-wq <eliahl1@elev.fryshuset.se>
2026-06-03 19:39:28 +0200

    f

--- src/game/agar/Game.jsx
+++ src/game/agar/Game.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect, useState } from 'react';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from '../../context/AuthContext';
 import { io } from 'socket.io-client';
 import global from './global.js';
 import Canvas from './canvas.js';
