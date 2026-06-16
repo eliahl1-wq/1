88d4a1f861c1c50f9a6572922f785347fa333dbe
eliahl1-wq <eliahl1@elev.fryshuset.se>
2026-06-16 09:23:06 +0200

    f

--- src/game/slither/SlitherGame.jsx
+++ src/game/slither/SlitherGame.jsx
@@ -548,7 +548,7 @@ export default function SlitherGame() {
 
 
 
-        inputIntervalRef.current = setInterval(emitInput, 25);
+        inputIntervalRef.current = setInterval(emitInput, 16);
 
 
 
